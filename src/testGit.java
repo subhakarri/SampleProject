06af679dd1b056b1ee93ba5fa81f1da54635a91c
subha <subha@LAPTOP-FEL77BLU>
2022-04-05 16:58:16 -0700

    First Commit.....

---
+++ src/testGit.java
@@ -0,0 +1,9 @@
+
+public class testGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" This is program will be inserted to local rep/Gti");
+	}
+
+}
